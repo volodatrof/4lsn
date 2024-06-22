25af5cc60e1a99d3cd51979bd3c8b0a89eb7d5f9
voloda <voloda@DESKTOP-H68HFR6>
2024-06-22 20:27:11 +0300

    .

---
+++ t2/Task2.js
@@ -0,0 +1,12 @@
+function updateClock() {
+    const clockElement = document.getElementById('clock');
+    const now = new Date();
+    const hours = String(now.getHours()).padStart(2, '0');
+    const minutes = String(now.getMinutes()).padStart(2, '0');
+    const seconds = String(now.getSeconds()).padStart(2, '0');
+    const timeString = `${hours}:${minutes}:${seconds}`;
+    clockElement.textContent = timeString;
+}
+
+setInterval(updateClock, 1000);
+updateClock();
